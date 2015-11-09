c25b2785d92222c8008cf16e8d0281d2acb92efa
FelixSellmair <felix.sellmair@gmail.com>
2015-11-09 13:46:32 +0100

    MapsActivity with Locations

--- app/src/main/java/com/unibz/serendipity/utilities/SoundListCreator.java
+++ app/src/main/java/com/unibz/serendipity/utilities/SoundListCreator.java
@@ -5,23 +5,16 @@ import com.unibz.serendipity.Sound;
 import java.util.ArrayList;
 
 /**
- * Created by Cody on 02.11.15.
+ * Created by Felix on 02.11.15.
  */
 public class SoundListCreator {
-    public static ArrayList<Sound> soundList;
+    public static ArrayList<Sound> soundList = new ArrayList<Sound>();
 
 
-    public  SoundListCreator() {
-        soundList = new ArrayList<Sound>();
 
-
-    }
     public void addSound(String name, double lat, double lon, String link) {
         Sound sound = new Sound(name,lat,lon,link);
         soundList.add(sound);
-    }
-    public ArrayList<Sound> getSoundList() {
-        return soundList;
     }
 
 }
