cc6bb8b756daf5c26297b08a80ce79af7f5f1cbd
Mercy Lio <mercy.lio95@gmail.com>
2015-11-18 19:12:52 +0100

    Sign in page added

--- app/src/main/java/com/unibz/serendipity/HomePageActivity.java
+++ app/src/main/java/com/unibz/serendipity/HomePageActivity.java
@@ -37,6 +37,21 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
 
         Button button3= (Button) findViewById(R.id.button3);
         button3.setOnClickListener(this);
+
+        Button button4= (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        Button button5= (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+        Button button6= (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        Button button7= (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        Button button8= (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(this);
     }
 
     public boolean onCreateOptionsMenu (Menu menu) {
@@ -56,23 +71,43 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
 
         if(v.getId() == R.id.button) {
             Log.i("clicks", "Sign In");
-            //Intent intent = new Intent(HomePageActivity.this, SignIn.class);
-            //startActivity(intent);
+            Intent intent = new Intent(HomePageActivity.this, SignInActivity.class);
+            startActivity(intent);
         }
         else if(v.getId() == R.id.button2) {
-            Log.i("clicks", "Rules");
-            Intent intent = new Intent(HomePageActivity.this, RulesActivity.class);
-            startActivity(intent);
+            Log.i("clicks", "Upload");
+            //Intent intent = new Intent(HomePageActivity.this, UploadActivity.class);
+            //startActivity(intent);
         }
         else if(v.getId() == R.id.button3) {
             Log.i("clicks", "About");
             Intent intent = new Intent(HomePageActivity.this, AboutActivity.class);
             startActivity(intent);
         }
-        else(v.getId() == R.id.button4) {
-            Log.i("clicks", "Contacts");
-            Intent intent = new Intent(HomePageActivity.this, ContactsActivity.class);
+        else if(v.getId() == R.id.button4) {
+            Log.i("clicks", "Cities");
+            //Intent intent = new Intent(HomePageActivity.this, CitiesActivity.class);
+            //startActivity(intent);
+        }
+        else if(v.getId() == R.id.button5) {
+            Log.i("clicks", "Maps");
+            Intent intent = new Intent(HomePageActivity.this, MapsActivity.class);
             startActivity(intent);
+        }
+        else if(v.getId() == R.id.button6) {
+            Log.i("clicks", "Listen");
+            //Intent intent = new Intent(HomePageActivity.this, ListenActivity.class);
+            //startActivity(intent);
+        }
+        else if(v.getId() == R.id.button7) {
+            Log.i("clicks", "Rules");
+            Intent intent = new Intent(HomePageActivity.this, RulesActivity.class);
+            startActivity(intent);
+        }
+        else(v.getId() == R.id.button8) {
+            Log.i("clicks", "Contacts");
+            //Intent intent = new Intent(HomePageActivity.this, ContatsActivity.class);
+            //startActivity(intent);
         }
 
     }
