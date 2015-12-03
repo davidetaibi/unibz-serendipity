a4990013eafd07517b8fdab3442e27e6afc57118
Mercy Lio <mercy.lio95@gmail.com>
2015-12-03 10:40:28 +0100

    New additions
    Register Activity added
    Sign In and Cities Activities edited but missing URL, session names and
    session id's

--- app/src/main/java/com/unibz/serendipity/CitiesActivity.java
+++ app/src/main/java/com/unibz/serendipity/CitiesActivity.java
@@ -1,29 +1,34 @@
 package com.unibz.serendipity;
 
+import android.content.Intent;
+import android.net.http.HttpResponseCache;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 public class CitiesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-    //    lv = (ListView) findViewById(R.id.soundList);
-
-       // ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-         //       (this, android.R.layout.simple_list_item_1, soundList);
-
-    //    lv.setAdapter(arrayAdapter);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new FetchItems().execute();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +38,48 @@ public class CitiesActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+    private class FetchItems extends AsyncTask<String, Void, JSONArray>{
+        protected JSONArray doInBackground(String... params){
+            HttpClient httpClient = new DefaultHttpClient();
+
+            HttpGet httpGet = new HttpGet("");
+            httpGet.setHeader("Accept","application/json");
+            httpGet.setHeader("Content-type","application/json");
+
+            JSONArray json = new JSONArray();
+
+            try{
+                HttpResponse response = httpClient.execute(httpGet);
+
+                json = new JSONArray(EntityUtils.toString(response.getEntity()));
+                return json;
+            }catch (Exception e){
+                Log.v("Error adding ",e.getMessage());
+            }return json;
+        }
+
+        protected void onPostExecute(JSONArray result){
+            ListView lv = (ListView) findViewById(R.id.listView);
+            ArrayList<String> listItems = new ArrayList<String>();
+
+            for (int i=0;i<result.length();i++){
+                try {
+                    listItems.add(result.getJSONObject(i).getString("this").toString());
+                }catch (Exception e){
+                    Log.v("Error adding :", e.getMessage());
+                }
+            }
+            ArrayAdapter ad = new ArrayAdapter(CitiesActivity.this,android.R.layout.simple_gallery_item);
+            lv.setAdapter(ad);
+        }
+    }
+
+
+    public void addCity_click(View view){
+
     }
 
 }
--- app/src/main/java/com/unibz/serendipity/SignInActivity.java
+++ app/src/main/java/com/unibz/serendipity/SignInActivity.java
@@ -1,13 +1,30 @@
 package com.unibz.serendipity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.os.AsyncTask;
+import android.widget.EditText;
+
+import org.apache.http.client.HttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SignInActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +43,53 @@ public class SignInActivity extends AppCompatActivity {
         });
     }
 
+    String session_name;
+    String session_id;
+    private class doSignIn extends AsyncTask<String, Integer, Long> {
+
+        protected Integer doInBackground(String... params){
+            HttpClient httpClient=new DefaultHttpClient();
+            HttpPost httpPost=new HttpPost("");
+
+            try {
+                EditText username = (EditText) findViewById(R.id.editName);
+                EditText password = (EditText) findViewById(R.id.editPassword);
+
+                JSONObject json = new JSONObject();
+                json put("username",username.getText().toString().trim());
+                json put("password",password.getText().toString().trim());
+
+                StringEntity se = new StringEntity(json.toString());
+                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                httpPost.setEntity(se);
+
+                HttpResponse response = httpClient.execute(httpPost);
+
+                String jsonResponse = EntityUtils.toString(response.getEntity());
+
+                JSONObject jsonObj = new JSONObject(jsonResponse);
+
+                session_name = jsonObj.getString("session_name");
+                session_id = jsonObj.getString("session_id");
+
+            }catch (Exception e){
+                Log.v("Error logging in :", e.getMessage());
+            }
+            return 0;
+        }
+        protected void onPostExecute(Long result){
+            Intent intent = new Intent(SignInActivity.this,CitiesActivity.class);
+
+            intent.putExtra("session_name",session_name);
+            intent.putExtra("session_id",session_id);
+
+            startActivity(intent);
+        }
+    }
+
+    public void doSignIn_click(View view){
+
+        new doSignIn().execute();
+
+    }
 }
