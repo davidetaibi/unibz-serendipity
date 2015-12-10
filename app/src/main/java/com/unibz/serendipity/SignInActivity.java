419e48f90150740b3f2588d62dacd16b1bb94a1e
Mercy Lio <mercy.lio95@gmail.com>
2015-12-10 18:10:53 +0100

    Updated signin

--- app/src/main/java/com/unibz/serendipity/SignInActivity.java
+++ app/src/main/java/com/unibz/serendipity/SignInActivity.java
@@ -4,6 +4,7 @@ package com.unibz.serendipity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +12,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.os.AsyncTask;
+import android.widget.Button;
 import android.widget.EditText;
 
-import org.apache.http.client.HttpClient;
+import com.loopj.android.http.*;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -23,7 +27,18 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SignInActivity extends AppCompatActivity {
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+import cz.msebera.android.httpclient.message.BasicHeader;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
 
 
@@ -33,6 +48,10 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button regButton= (Button) findViewById(R.id.regButton);
+        regButton.setOnClickListener(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,38 +65,137 @@ public class SignInActivity extends AppCompatActivity {
 
     String session_name;
     String session_id;
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.regButton){
+            Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+            startActivity(intent);
+    }else{
+            return;
+        }
+    }
+
     private class doSignIn extends AsyncTask<String, Integer, Long> {
 
-        protected Integer doInBackground(String... params){
-            HttpClient httpClient=new DefaultHttpClient();
-            HttpPost httpPost=new HttpPost("");
 
-            try {
-                EditText username = (EditText) findViewById(R.id.editName);
-                EditText password = (EditText) findViewById(R.id.editPassword);
+        private static final String BASE_URL = "https://our.drupal.webservices.com";
+        private final AsyncHttpClient client = new AsyncHttpClient();
 
-                JSONObject json = new JSONObject();
-                json put("username",username.getText().toString().trim());
-                json put("password",password.getText().toString().trim());
+//        private static AsyncHttpClient client = new AsyncHttpClient();
 
-                StringEntity se = new StringEntity(json.toString());
-                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-                httpPost.setEntity(se);
+        protected Long doInBackground(String... params){
 
-                HttpResponse response = httpClient.execute(httpPost);
 
-                String jsonResponse = EntityUtils.toString(response.getEntity());
+//            client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+//
+//                @Override
+//                public void onStart() {
+//                    // called before request is started
+//                }
+//
+//                @Override
+//                public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+//                    // called when response HTTP status is "200 OK"
+//                }
+//
+////                @Override
+////                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+////                    // If the response is JSONObject instead of expected JSONArray
+////                }
+////
+////                @Override
+////                public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+////                    // Pull out the first event on the public timeline
+////                    JSONObject firstEvent = timeline.get(0);
+////                    String tweetText = firstEvent.getString("text");
+////
+////                    // Do something with the response
+////                    System.out.println(tweetText);
+////                }
+//
+//                @Override
+//                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+//                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+//                }
+//
+//                @Override
+//                public void onRetry(int retryNo) {
+//                    // called when request is retried
+//                }
+//            });
 
-                JSONObject jsonObj = new JSONObject(jsonResponse);
+            //Usage
+            get("statuses/public_timeline.json", null, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    // If the response is JSONObject instead of expected JSONArray
+                    System.out.println("JSONObject response: " + response.toString());
+                }
 
-                session_name = jsonObj.getString("session_name");
-                session_id = jsonObj.getString("session_id");
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                    // Pull out the first event on the public timeline
+                    String text = "";
+                    try {
+                        JSONObject firstEvent = (JSONObject) timeline.get(0);
+                        text = firstEvent.getString("text");
+                    } catch (Exception e) { e.printStackTrace(); }
+                    // Do something with the response
+                    System.out.println(text);
+                }
+            });
 
-            }catch (Exception e){
-                Log.v("Error logging in :", e.getMessage());
-            }
-            return 0;
+
+
+
+
+
+            return new Long(0);
+//            HttpClient httpClient=new HttpClientBuilder.create().build();
+//            HttpPost httpPost=new HttpPost("");
+//
+//            try {
+//                EditText username = (EditText) findViewById(R.id.editName);
+//                EditText password = (EditText) findViewById(R.id.editPassword);
+//
+//                JSONObject json = new JSONObject();
+////                json.put("username", username.getText().toString().trim());
+////                json put("password",password.getText().toString().trim());
+//
+//                json.put("username", params[0]);
+//                json.put("password", params[1]);
+//                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+//                httpPost.setEntity(se);
+//
+//                HttpResponse response = httpClient.execute(httpPost);
+//
+//                String jsonResponse = EntityUtils.toString(response.getEntity());
+//
+//                JSONObject jsonObj = new JSONObject(jsonResponse);
+//
+//                session_name = jsonObj.getString("session_name");
+//                session_id = jsonObj.getString("session_id");
+//
+//            }catch (Exception e){
+//                Log.v("Error logging in :", e.getMessage());
+//            }
+//            return new Long(0);
         }
+
+        public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        private String getAbsoluteUrl(String relativeUrl) {
+            return BASE_URL + relativeUrl;
+        }
+
+
         protected void onPostExecute(Long result){
             Intent intent = new Intent(SignInActivity.this,CitiesActivity.class);
 
@@ -90,7 +208,14 @@ public class SignInActivity extends AppCompatActivity {
 
     public void doSignIn_click(View view){
 
-        new doSignIn().execute();
+        EditText username = (EditText) findViewById(R.id.editName);
+        EditText password = (EditText) findViewById(R.id.editPassword);
+
+
+
+        new doSignIn().execute(username.getText().toString(), password.getText().toString());
 
     }
+
+
 }
