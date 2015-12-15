434374bee53487af6de058b02583c335e37c18f9
Mercy Lio <mercy.lio95@gmail.com>
2015-12-15 16:27:20 +0100

    cityactivity edits

--- app/src/main/java/com/unibz/serendipity/CitiesActivity.java
+++ app/src/main/java/com/unibz/serendipity/CitiesActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.loopj.android.http.*;
@@ -25,10 +27,6 @@ import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class CitiesActivity extends AppCompatActivity {
 
-
-    String session_name;
-    String session_id;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -37,12 +35,8 @@ public class CitiesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Bundle extras=getIntent().getExtras();
-        session_name=extras.getString("session_name");
-        session_id=extras.getString("session_id");
-
-        new FetchItems().execute();
-
+        Button addNew= (Button) findViewById(R.id.addNew);
+        addNew.setOnClickListener((View.OnClickListener) this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -54,51 +48,59 @@ public class CitiesActivity extends AppCompatActivity {
         });
     }
 
+    private long mLastClickTime=0;
+    //@Override
+    public void onClick(View v) {
 
-    private class FetchItems extends AsyncTask<String, Void, JSONArray>{
-        protected JSONArray doInBackground(String... params){
-            HttpClient httpClient = new DefaultHttpClient();
 
-            HttpGet httpGet = new HttpGet("");
-            httpGet.setHeader("Accept","application/json");
-            httpGet.setHeader("Content-type","application/json");
-
-            JSONArray json = new JSONArray();
-
-            try{
-                HttpResponse response = httpClient.execute(httpGet);
-
-                json = new JSONArray(EntityUtils.toString(response.getEntity()));
-                return json;
-            }catch (Exception e){
-                Log.v("Error adding ",e.getMessage());
-            }return json;
+        if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
+            return;
         }
+        mLastClickTime = SystemClock.elapsedRealtime();
 
-        protected void onPostExecute(JSONArray result){
-            ListView lv = (ListView) findViewById(R.id.listView);
-            ArrayList<String> listItems = new ArrayList<String>();
+        if (v.getId() == R.id.addNew) {
+            Intent intent = new Intent(CitiesActivity.this, UploadActivity.class);
+            startActivity(intent);}
 
-            for (int i=0;i<result.length();i++){
-                try {
-                    listItems.add(result.getJSONObject(i).getString("this").toString());
-                }catch (Exception e){
-                    Log.v("Error adding :", e.getMessage());
-                }
-            }
-            ArrayAdapter ad = new ArrayAdapter(CitiesActivity.this,android.R.layout.simple_gallery_item);
-            lv.setAdapter(ad);
-        }
     }
 
+    protected void onPostExecute(JSONArray result) {
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayList<String> cityList = (ArrayList<String>) getIntent().getSerializableExtra("cityList");
 
-    public void addCity_click(View view){
-            Intent intent = new Intent(this,UploadActivity.class);
+//private class FetchItems extends AsyncTask<String, Void, JSONArray>{
+//        protected JSONArray doInBackground(String... params){
+//            HttpClient httpClient = new DefaultHttpClient();
+//
+//            HttpGet httpGet = new HttpGet("");
+//            httpGet.setHeader("Accept","application/json");
+//            httpGet.setHeader("Content-type","application/json");
+//
+//            JSONArray json = new JSONArray();
+//
+//            try{
+//                HttpResponse response = httpClient.execute(httpGet);
+//
+//                json = new JSONArray(EntityUtils.toString(response.getEntity()));
+//                return json;
+//            }catch (Exception e){
+//                Log.v("Error adding ",e.getMessage());
+//            }return json;
+//        }
+//
+//        protected void onPostExecute(JSONArray result){
+//            ListView lv = (ListView) findViewById(R.id.listView);
+//            ArrayList<String> listItems = new ArrayList<String>();
+//
+//            for (int i=0;i<result.length();i++){
+//                try {
+//                    listItems.add(result.getJSONObject(i).getString("this").toString());
+//                }catch (Exception e){
+//                    Log.v("Error adding :", e.getMessage());
+//                }
+//            }
+//            ArrayAdapter ad = new ArrayAdapter(CitiesActivity.this,android.R.layout.simple_gallery_item);
+//            lv.setAdapter(ad);
+//        }
 
-            intent.putExtra("session_name",session_name);
-            intent.putExtra("session_id",session_id);
-
-            startActivity(intent);
-    }
-
-}
+    }}
