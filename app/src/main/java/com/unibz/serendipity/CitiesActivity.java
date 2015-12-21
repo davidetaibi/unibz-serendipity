73ac03d8a3e9735ccdd5c15b322abc1f4bd22476
Mercy Lio <mercy.lio95@gmail.com>
2015-12-21 14:10:18 +0100

    toolbar fixes and lorem ipsum

--- app/src/main/java/com/unibz/serendipity/CitiesActivity.java
+++ app/src/main/java/com/unibz/serendipity/CitiesActivity.java
@@ -32,20 +32,11 @@ public class CitiesActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Button addNew= (Button) findViewById(R.id.addNew);
         addNew.setOnClickListener((View.OnClickListener) this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     private long mLastClickTime=0;
