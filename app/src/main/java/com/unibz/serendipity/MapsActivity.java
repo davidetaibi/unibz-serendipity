b349dbc5c6dbc1317e29f95b14c21fc7f05fc148
FallenRiteMonk <fallenritemonk@gmail.com>
2015-12-21 17:41:12 +0100

    maps activity follow user

--- app/src/main/java/com/unibz/serendipity/MapsActivity.java
+++ app/src/main/java/com/unibz/serendipity/MapsActivity.java
@@ -1,19 +1,34 @@
 package com.unibz.serendipity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.unibz.serendipity.utilities.GPSTracker;
 import com.unibz.serendipity.utilities.SoundList;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 public class MapsActivity extends AppCompatActivity {
+    private final String LOG_TAG = "MAPS_ACTIVITY";
+
     MapView mapView;
     GoogleMap map;
+    private BroadcastReceiver locationChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +47,48 @@ public class MapsActivity extends AppCompatActivity {
         // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
         MapsInitializer.initialize(this);
 
-        // Updates the location and zoom of the MapView
-        //CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        //map.animateCamera(cameraUpdate);
-
         for(int i=0;i< SoundList.soundList.size();i++) {
             LatLng currLoc = new LatLng(SoundList.soundList.get(i).getLatitude(),SoundList.soundList.get(i).getLongitude());
             MarkerOptions currOpts = new MarkerOptions().position(currLoc).title(SoundList.soundList.get(i).getTitle());
             map.addMarker(currOpts);
             map.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
         }
+
+        locationChangeReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Location newLocation = intent.getParcelableExtra(GPSTracker.EXTRA);
+                if (newLocation != null) {
+                    locationChanged(newLocation);
+                }
+            }
+        };
+
+        loadLastKnown();
+    }
+
+    private void loadLastKnown() {
+        Location currentLocation = GPSTracker.getCurrentLocation();
+        if (currentLocation != null) {
+            locationChanged(currentLocation);
+        }
+    }
+
+    public void locationChanged(Location location) {
+        //Toast.makeText(this, "LocationChanged", Toast.LENGTH_LONG).show();
+        //Log.d(LOG_TAG, "Location changed: lat: " + location.getLatitude() + "  long: " + location.getLongitude());
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15);
+        map.animateCamera(cameraUpdate);
     }
 
     @Override
     public void onResume() {
+        loadLastKnown();
+
+        IntentFilter filter = new IntentFilter(GPSTracker.ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(locationChangeReceiver, filter);
+
         mapView.onResume();
         super.onResume();
     }
@@ -54,6 +97,7 @@ public class MapsActivity extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         mapView.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(locationChangeReceiver);
     }
 
     @Override
